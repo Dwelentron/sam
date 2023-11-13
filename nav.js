c1371cdf5918b02e3f134f96f8e85c3768f5b543
Dwelentron <150653540+Dwelentron@users.noreply.github.com>
2023-11-13 11:27:44 +0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,10 @@
+// const sam=document.getElementsByClassName('button')[0]
+// const navbar=document.getElementsByClassName('five')[0]
+// sam.addEventListener('click', ()=>{
+//     navbar.classList.toggle('active')
+// })
+const sm=document.getElementsByClassName('button')[0]
+const navbr=document.getElementsByClassName('ten')[0]
+sm.addEventListener('click', ()=>{
+    navbr.classList.toggle('active')
+})
---
+++ script.js
@@ -0,0 +1,5 @@
+const tango=document.getElementsByClassName('button')[0];
+const navbar=document.getElementsByClassName('four')[0];
+navbar.addEventListener('click', () =>{
+    navbar.classList.toggle('active')
+})
